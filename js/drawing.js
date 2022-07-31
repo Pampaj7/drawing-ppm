586c21108875d2b6894207f41b6308d78e6d4344
Pampaj7 <leonardo.pampaloni1@stud.unifi.it>
2022-07-31 18:09:15 +0200

    Added Draw-Function

--- js/drawing.js
+++ js/drawing.js
@@ -3,7 +3,7 @@ var context;
 var coloreSelezionato;
 var LarghezzaLinea = 2;
 var puntoInizioDisegnoX = null;
-var PuntoInizioDisegnoY = null;
+var puntoInizioDisegnoY = null;
 var posizioneCorrenteMouseX = null;
 var posizioneCorrenteMouseY = null;
 var mouseIsDown = false;
@@ -11,7 +11,7 @@ var mouseIsDown = false;
 document.addEventListener(
     'DOMContentLoaded',
     function () {
-        console.log('DOMContentLoaded')  ;
+        console.log('DOMContentLoaded');
         canvas = document.getElementById('areaDiDisegno');
         context = canvas.getContext('2d');
 
@@ -27,9 +27,72 @@ document.addEventListener(
                     'click',
                     function () {
                         var id = this.id;
+                        var colore = id.match(/[A-Z][a-z]+/g);
+
+                        if (id == 'bottoneCancella') {
+                            cancella();
+                        } else {
+                            selezionaColore(
+                                {
+                                    colore: colore[0].toLowerCase()
+                                }
+                            );
+                        }
                     }
                 )
             }
-        )
+        );
+        canvas.addEventListener('mousemove', function (e) {
+            calcolaCoordinate(e);
+        });
+        canvas.addEventListener('mousedown', function (e) {
+            calcolaCoordinate(e);
+        });
+        canvas.addEventListener('mouseup', function (e) {
+            calcolaCoordinate(e);
+        });
+        canvas.addEventListener('mouseout', function (e) {
+            calcolaCoordinate(e);
+        });
+
+
     }
 );
+
+function selezionaColore(obj) {
+    coloreSelezionato = obj.colore;
+}
+
+function calcolaCoordinate(e) {
+    console.log(e.type);
+
+    switch (e.type) {
+        case 'mousemove' :
+            if (mouseIsDown) {
+                posizioneCorrenteMouseX = e.clientX - canvas.offsetLeft;
+                posizioneCorrenteMouseY = e.clientY - canvas.offsetTop;
+                //disegna();
+                puntoInizioDisegnoX = posizioneCorrenteMouseX;
+                puntoInizioDisegnoY = posizioneCorrenteMouseY;
+            }
+            break;
+        case 'mousedown' :
+            mouseIsDown = true;
+            puntoInizioDisegnoX = posizioneCorrenteMouseX;
+            puntoInizioDisegnoY = posizioneCorrenteMouseY;
+            break;
+        case 'mouseup' :
+            mouseIsDown = false;
+
+            break;
+        case 'mouseout' :
+            mouseIsDown = false;
+            break;
+    }
+}
+
+function disegna () {
+    context.beginPath();
+    context.moveTo(puntoInizioDisegnoX, puntoInizioDisegnoY);
+}
+
